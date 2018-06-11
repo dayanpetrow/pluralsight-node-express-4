155be169a49bec88beebe8b0ef411987965bffed
Dayan Petrov <dayan.petroff@gmail.com>
2018-06-11 13:50:16 +0300

    course finished

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -2,23 +2,12 @@ const express = require("express");
 const adminRouter = express.Router();
 const { MongoClient } = require("mongodb");
 const debug = require("debug")("app:adminRoutes");
+const bookService = require('../services/goodreadsService');
+
+const MAX_BOOK_ID = 41000000;
 
 function router(nav) {
-  const books = [
-    {
-      bookId: 1,
-      title: "dido"
-    },
-    {
-      bookId: 320,
-      title: "petrow"
-    },
-    {
-      bookId: 550,
-      title: "petrow"
-    }
-  ];
-  adminRouter.route("/").get((req, res) => {
+  adminRouter.route("/add_random_book").get((req, res) => {
     const url = "mongodb://localhost:27017";
     const dbName = "libraryApp";
     (async function mongo() {
@@ -29,8 +18,20 @@ function router(nav) {
 
         const db = client.db(dbName);
 
-        const response = await db.collection("books").insertMany(books);
-        res.json(response);
+        let random_book = Math.random() * MAX_BOOK_ID;
+
+        const book_result = await bookService.getBookById(random_book);
+
+        const book = {
+            bookId: `${book_result.id}`,
+            title: `${book_result.title}`,
+            img: `${book_result.image_url}`
+        }
+
+        const collection = await db.collection("books");
+        const response = await collection.insertOne(book);
+        res.json(book_result);
+        //res.redirect('/books')
       } catch (err) {
         debug(err.stack);
       }
